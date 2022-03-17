3ce5f337bbead4579b722edb45054d3d36f3d1f4
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-03-17 06:37:56 +0530

    Delete example directory

--- example/searching.py
+++
@@ -1,7 +0,0 @@
-from pypi_scrapped import pypi_search
-project = input ("Type something ")
-result = pypi_search (project)
-if not result:
-  print ("nothing found on pypi")
-else:
-  print (result)
